a77ab8fb18ef587e086c0d916d59a12a1307c30f
shams alam <103259015+shams220@users.noreply.github.com>
2025-10-29 13:05:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-// complete this js code
-class Person{
-    constructor(name,age){
-        this.name = name;
-        this.age = age;
-    }
-    greet(){
-        console.log(`Hello my name is ${this.name}, I am ${this.age} years old.`);
-    }
-}
-class Employee extends Person{
-    constructor(name,age,jobtitle){
-        super(name,age)
-        this.jobtitle = jobtitle ; 
-    }
-    jobGreet(){
-        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobtitle}`);
-    }
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  greet() {
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+  }
 }
 
-let p = new Person("Alice",25);
-p.greet()
-let e = new Employee("Alice",25,"web D");
-e.jobGreet()
+class Employee extends Person {
+  constructor(name, age, jobtitle) {
+    super(name, age);
+    this.jobtitle = jobtitle;
+  }
+  jobGreet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobtitle}.`);
+  }
+}
+
+let p = new Person("Alice", 25);
+p.greet();
+
+let e = new Employee("Alice", 25, "web D");
+e.jobGreet();
 
 // Do not change code below this line
 window.Person = Person;
