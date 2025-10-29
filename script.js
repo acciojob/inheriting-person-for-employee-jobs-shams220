761339cffde2791afef09e31eefeb3d9c32ee421
shams alam <103259015+shams220@users.noreply.github.com>
2025-10-29 12:57:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 // complete this js code
-function Person(name, age) {}
+class Person{
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
+    greet(){
+        console.log(`Hello my name is ${this.name}, I am ${this.age} years old.`);
+    }
+}
+class Employee extends Person{
+    constructor(name,age,jobtitle){
+        this.name = name;
+        this.age = age;
+        this.jobtitle = jobtitle ; 
+    }
+    jobGreet(){
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobtitle}`);
+    }
+}
 
-function Employee(name, age, jobTitle) {}
+let p = new Person("Alice",25);
+p.greet()
+let e = new Employee("Alice",25,"web D");
+e.jobGreet()
 
 // Do not change code below this line
 window.Person = Person;
